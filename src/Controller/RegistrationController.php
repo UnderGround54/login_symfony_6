26091bce788b34f0debbd47e119055ceba69033e
UnderGround54 <martinpros54@gmail.com>
2022-08-23 17:23:08 +0300

    modification et suppression

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,15 +4,16 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
 use App\Security\UserAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
@@ -45,6 +46,60 @@ class RegistrationController extends AbstractController
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    #[Security("is_granted('ROLE_USER')")]
+    #[Route('/liste', name: 'user.list')]
+    public function index(UserRepository $repository): Response
+    {
+        return $this->render('liste/index.html.twig', [
+            'users' => $repository->findAll(),
+        ]);
+    }
+
+    #[Security("is_granted('ROLE_USER')")]
+    #[Route('/suppression/{id}', name: 'user.delete', methods: 'GET')]
+    public function delete(EntityManagerInterface $manager, User $user): Response
+    {
+        if (!$user) {
+            $this->addFlash(
+                'warning',
+                'Utilisateur n\'a pas été trouvé'
+            );
+            return $this->redirectToRoute('user.list');
+        } else {
+            $manager->remove($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'succes',
+                'Votre Utilisateur à été supprimer avec succes !'
+            );
+            return $this->redirectToRoute('user.list');
+        }
+    }
+    #[Security("is_granted('ROLE_USER')")]
+    #[Route('/modification/{id}', name:'user.update', methods:['GET','POST'])]
+    public function update(EntityManagerInterface $manager, User $user, Request $request): Response
+    {
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $user = $form->getData();
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'succes',
+                'Votre Utilisateur à été modifier avec succes !'
+            );
+            return $this->redirectToRoute('user.list');
+        }
+
+        return $this->render('registration/update.html.twig', [
+            'updateForm' => $form->createView(),
         ]);
     }
 }
